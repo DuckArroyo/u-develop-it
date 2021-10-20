e0fb60ad433b51f2c43c175ffd768c7fc4489bec
Rafael "Duck" Arroyo <87622570+DuckArroyo@users.noreply.github.com>
2021-10-20 15:27:27 -0700

    connected db to server

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const mysql = require("mysql2");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -6,6 +7,23 @@ const app = express();
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+// Connect to database
+const db = mysql.createConnection(
+  {
+    host: "localhost",
+    // Your MySQL username,
+    user: "root",
+    // Your MySQL password
+    password: "Omega!519",
+    database: "election",
+  },
+  console.log("Connected to the election database.")
+);
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+  //!Dont delete this console log. Console logs here post to the CLI. The console log is not funtional, I dont want to forget it's use.
+  console.log(rows);
+});
 
 app.get("/", (req, res) => {
   res.json({
